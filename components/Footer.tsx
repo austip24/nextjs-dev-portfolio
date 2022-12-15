f701fc6ebbeb49076b61b21a488344772d95aee1
austip24 <101380606+austip24@users.noreply.github.com>
2022-12-15 13:32:15 -0700

    Delete Footer.tsx

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export const Footer: React.FC = () => {
-	return (
-		<footer className="h-footer-height text-neutral text-sm w-full grid place-items-center text-center px-4">
-			Copyright &copy; 2022, Austin Pierson. All Rights Reserved.
-		</footer>
-	);
-};
